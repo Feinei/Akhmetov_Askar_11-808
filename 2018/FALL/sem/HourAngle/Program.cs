d8296724e26d90cee6c31ad7ca9954b75b944c3a
Askar <axmetov.ackap@rambler.ru>
2018-09-12 13:15:39 +0300

    2018

---
+++ 2018/FALL/sem/HourAngle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace firstApp
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int angle;
+            string hour = Console.ReadLine();
+            int hourint = int.Parse(hour);
+            if (hourint <= 6)
+            {
+                angle = hourint * 30;
+            }
+            else
+            {
+                angle = 360 - hourint * 30;
+            }
+            Console.WriteLine(angle);
+            Console.ReadKey();
+        }        
+    }
+}
