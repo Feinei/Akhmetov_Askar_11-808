482bbbc7573639753dd148c3ee3a4287657ea3aa
Askar <axmetov.ackap@rambler.ru>
2018-09-13 11:46:38 +0300

    task from ulearn

---
+++ 2018/FALL/pr/BankPercent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankPercent
+{
+    class Program
+    {
+        public static double Calculate(string userInput)
+        {
+            var parts = userInput.Split();
+            var money = double.Parse(parts[0]);
+            var percent = double.Parse(parts[1]);
+            var month = int.Parse(parts[2]);
+            double monthPercent = 1 + percent / 1200;
+            money = money * monthPercent;
+            return (money * Math.Pow(monthPercent, month - 1));
+        }
+
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Calculate(userInput));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/FALL/pr/Billiard/BilliardsTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направелния движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            double direction = (directionRadians * 180) / Math.PI;
+            double wallInclination = (wallInclinationRadians * 180) / Math.PI;
+            // по формуле x = 2b-a 
+            // a - угол между направлением удара и осью Х
+            // b - угол между стеной и осью Х
+            // x - угол отскока 
+            return (2 * wallInclination - direction) * Math.PI / 180;
+        }
+    }
+}
