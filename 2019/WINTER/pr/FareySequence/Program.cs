2fb0e1c20ffaad2b148384ab52ce02ed767bc6b4
Askar <42945205+Feinei@users.noreply.github.com>
2019-02-12 12:09:23 +0300

    Add files via upload

---
+++ 2019/WINTER/pr/FareySequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareySequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seqOrder = int.Parse(Console.ReadLine());
+            var farSequence = new FarSequence(seqOrder);
+            farSequence.FillSequence();
+            farSequence.Print();
+            Console.ReadKey();
+        }
+    }
+}
